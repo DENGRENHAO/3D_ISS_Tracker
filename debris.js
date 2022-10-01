9edc7a848f23ee9cd1df26624759ef74427b1978
thomas <ddeng691@gmail.com>
2022-10-01 18:20:49 +0800

    fix: update debris

--- debris.js
+++ debris.js
@@ -1,23 +1,49 @@
-function drawDebris(){
+var allDebris = [];
+var allSatrec = [];
+
+function drawDebris(time){
     fetch("/Nasa_Space_App/data/satellites.json")
     .then(response => response.json())
     .then((json) => {
-        var debrisLayer = new WorldWind.RenderableLayer("Debris Layer");
-        wwd.addLayer(debrisLayer);
-        var time = get_render_time();
-        for(let i = 0; i < json.length; i++){
-            if(json[i].OBJECT_TYPE=='DEBRIS'){
-                var tleLine1 = json[i].TLE_LINE1,
-                    tleLine2 = json[i].TLE_LINE2;    
-
-                var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
-                var pos = get_iss_pos(satrec, time);
-                debrisLayer.addRenderable(new WorldWind.Placemark(
-                    new WorldWind.Position(pos[0], pos[1], pos[2])
-                ));
+        if(allDebris.length==0){
+            for(let i = 0; i < json.length; i++){
+                if(json[i].OBJECT_TYPE=='DEBRIS'){
+                    var tleLine1 = json[i].TLE_LINE1,
+                        tleLine2 = json[i].TLE_LINE2;    
+    
+                    var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
+                    allSatrec.push(satrec);
+                    var pos = get_iss_pos(satrec, time);
+                    var debris = new WorldWind.Placemark(
+                        new WorldWind.Position(pos[0], pos[1], pos[2])
+                    );
+                    debrisLayer.addRenderable(debris);
+                    allDebris.push(debris);
+                }
             }
         }
     });
 }
 
-drawDebris();
+var time = get_render_time();
+var debrisLayer = new WorldWind.RenderableLayer("Debris Layer");
+wwd.addLayer(debrisLayer);
+setTimeout(drawDebris(), 1000);
+
+function updateDebris(){
+    var time = get_render_time();
+    for(let i = 0; i < allDebris.length; i++){
+        var pos = get_iss_pos(allSatrec[i], time)
+        allDebris[i].position['latitude'] = pos[0];
+        allDebris[i].position['longitude'] = pos[1];
+        allDebris[i].position['altitude'] = pos[2];
+        modelLayer.refresh();
+        wwd.redraw();
+    }
+}
+
+setInterval(updateDebris, 3000);
+
+function toggleDebris(){
+    debrisLayer.enabled = !debrisLayer.enabled;
+}
