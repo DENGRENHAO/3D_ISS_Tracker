ddf6fbb086194de8579dcda3d976fa10d1f31f10
thomas <ddeng691@gmail.com>
2022-10-02 10:25:58 +0800

    fix: update formula

--- sph2car.js
+++ sph2car.js
@@ -1,7 +1,7 @@
 function sph2car(la, long, hei) {
     r = hei + 6371;
-    phi = (90 - la) * 360 / (2*Math.PI);
-    theta = long * 360 / (2*Math.PI);
+    phi = (90 - la) / 360 * (2*Math.PI);
+    theta = long / 360 * (2*Math.PI);
     
     x = r*Math.sin(theta)*Math.cos(phi);
     y = r*Math.sin(theta)*Math.sin(phi)
